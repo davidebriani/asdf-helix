33c18faa8102a1efa4fc477b1b6aa18df7c2e6d5
Davide Briani <davide@briani.dev>
2023-10-25 22:17:36 +0200

    Configure installation script
    Adapt default template to list and download releases from the GitHub
    repository of Helix.
    
    Signed-off-by: Davide Briani <davide@briani.dev>

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for helix.
 GH_REPO="https://github.com/helix-editor/helix"
 TOOL_NAME="helix"
 TOOL_TEST="hx --version"
@@ -26,13 +25,10 @@ sort_versions() {
 
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+		grep -o 'refs/tags/.*' | cut -d/ -f3-
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if helix has other means of determining installable versions.
 	list_github_tags
 }
 
@@ -41,8 +37,24 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for helix
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	case $(uname -s) in
+	Darwin) os="macos" ;;
+	Windows) os="windows" ;;
+	*) os="linux" ;;
+	esac
+
+	case $(uname -m) in
+	x86_64) arch="x86_64" ;;
+	aarch64) arch="arm64" ;;
+	*) arch="other" ;;
+	esac
+
+	if [ "$arch" = "other" ]; then
+		echo "Unsupported architecture $(uname -m). Only x86_64 and ARM binaries are available."
+		exit
+	fi
+
+	url="$GH_REPO/releases/download/${version}/helix-${version}-${arch}-${os}.tar.xz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -60,8 +72,8 @@ install_version() {
 	(
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+		chmod +x "$install_path/hx"
 
-		# TODO: Assert helix executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
